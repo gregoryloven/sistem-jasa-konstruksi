55ec578801db7a09d9e09707d1ea7e89df65416b
gregoryloven <gregoryseminari@gmail.com>
2024-08-09 10:46:44 +0800

    laravel ui, sweetalert, layout admin

---
+++ resources/views/house_type/index.blade.php
@@ -0,0 +1,21 @@
+@extends('layouts_admin.admin')
+
+@push('css')
+<style>
+    #myTable td {text-align: center; vertical-align: middle;}
+</style>
+@endpush
+
+@section('content')
+
+
+
+
+
+
+@endsection
+
+@section('javascript')
+<script>
+
+@endsection
---
+++ resources/views/layouts_admin/sidebar.blade.php
@@ -0,0 +1,12 @@
+<div class="main-sidebar sidebar-style-2">
+        <aside id="sidebar-wrapper">
+            <div class="sidebar-brand">
+            <a href="/">Sistem e-Katalog</a>
+            </div>
+            <ul class="sidebar-menu">
+                <li><a class="nav-link" href="/"><i class="fas fa-fire"></i> <span>Dashboard</span></a></li>
+                <li><a class="nav-link" href="/product"><i class="fas fa-list-alt"></i> <span>Produk</span></a></li>
+            </ul>
+            
+        </aside>
+    </div>
